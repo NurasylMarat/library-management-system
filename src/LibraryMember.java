c1a2645b3160c1bfe524e246c413ac3ed9e679d4
Nur <nurasylmarat07@gmail.com>
2025-12-17 00:38:39 +0500

    Initial commit

---
+++ src/LibraryMember.java
@@ -0,0 +1,21 @@
+public class LibraryMember {
+    private String name;
+    private int memberId;
+
+    public LibraryMember(String name, int memberId){
+        this.name = name;
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void displayInfo() {
+        System.out.println("Member: " + name + "; ID: " + memberId);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    static void main(String[] args) {
+        Book book1 = new Book("Clean Code", "Robert Martin", 2008, true);
+        Book book2 = new Book("Java Basics", "James Gosling", 2015, false);
+
+        LibraryMember member1 = new LibraryMember("Dias", 101);
+        LibraryMember member2 = new LibraryMember("Medet", 102);
+
+        Library library = new Library("Central City Library");
+
+        library.displayInfo();
+
+        member1.displayInfo();
+        member2.displayInfo();
+
+        book1.displayInfo();
+        book2.displayInfo();
+
+        if(book1.getYear() > book2.getYear()){
+            System.out.println(book1.getTitle() + " is newer than " + book2.getTitle());
+        } else {
+            System.out.println(book2.getTitle() + " is newer than " + book1.getTitle());
+        }
+    }
+}
